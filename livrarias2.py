2c01725bb6648c20c795af78e7995a38bb24e3d8
João <jcoliveira160499@gmail.com>
2020-02-18 15:49:22 -0300

    Terminal + Adicionando livrarias externas

---
+++ livrarias2.py
@@ -0,0 +1,8 @@
+import numpy 
+import datetime as dt
+
+#Para que as livrarias sejam executadas corretamente devemos executar no terminal:
+#python 3 -m venv env
+#source env/bin/activate
+#pip install numpy
+#pip install datetime
